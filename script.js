285e8da25e42433376af2418bd5ac150927201be
introloud <s_malla156680@fanshaweonline.ca>
2022-10-14 18:29:17 -0400

    changed the rating colors

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //TMDB
 const API_KEY = 'api_key=79514b1bff00ab3f2719c1db1371e9b9';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
-                + API_KEY;
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
 const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
@@ -35,16 +34,6 @@ const showMovies = (data => {
   });
 });
 
-const getRatingColor = (vote => {
-  if(vote >= 8) {
-    return 'green';
-  } else if (vote >= 5) {
-    return 'orange';
-  } else {
-    return 'red';
-  }
-});
-
 const getMovies = (url => {
   fetch(url).then(res => res.json()).then(data => {
     console.log(data.results);
@@ -52,6 +41,20 @@ const getMovies = (url => {
   });
 });
 getMovies(API_URL);
+
+/**
+ * \getRatingColor
+ * simply changes the background color of the rating block to distinguish from good ratings to worst
+*/
+const getRatingColor = (vote => {
+  if(vote >= 7) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
